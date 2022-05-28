0eb9d8a53a0c0d3b64f059917db8551cad254267
John Leeds <leedsjohn6@gmail.com>
2022-05-28 17:16:42 -0500

    Worked on pickAlg

--- AlgTrainer.py
+++ AlgTrainer.py
@@ -18,15 +18,17 @@ Box 3:
 Box 4:
     algorithms that the user got correctly during the review session
 """
-
+import random
 import Box
 import WrongBox
 
 REVIEW_COUNT = 8
+BOX_2_PERCENTAGE = 70 # what percent of the time to pick from box 2 if there are algorithms in both box 2 and 3
 
 class AlgTrainer:
     def __init__(self):
         self.boxes = self.createBoxes()
+        self.queuedAlgorithms = []
         self.lastAlg = ""
 
     def pickAlg(self):
@@ -40,11 +42,62 @@ class AlgTrainer:
             If one of the boxes is empty, picks an algorithm from the other
         """
         minWrongBox = self.boxes[1].getMin()
+        notMinBox = self._pickAlgorithmNotMin()
         if self.boxes[3].length() >= REVIEW_COUNT:
-            self.review_session()
+            self._review_session()
         elif minWrongBox:
             return minWrongBox
-        elif self.box[1].length() != 
+        elif notMinBox:
+            return notMinBox
+        else:
+            print("Uhhh fix this")
+            return
 
-    def review_session(self):
+    def _pickAlgorithmNotMin(self):
+        """
+        _pickAlgorithmNotMin
+        Selects an algorithm when there is not one to select from box 1
+        """
+        box_selection = random.randrange(0, 100)
+
+        box2HasOption = self.boxes[2].length() != 0 and self.boxes[2].getFirstAlgorithmName() != self.lastAlg
+        box3HasOption = self.boxes[3].length() != 0 and self.boxes[3].getFirstAlgorithmName() != self.lastAlg
+
+        if box2HasOption and (not box3HasOption or box_selection <= BOX_2_PERCENTAGE):
+            return self.boxes[2].getAlgorithm()
+        elif box3HasOption and (not box2HasOption or box_selection > BOX_2_PERCENTAGE):
+            return self.boxes[3].getAlgorithm()
+        
+        return None
+
+    def _insertAlg(self, boxIndex, alg):
+        """
+        _insertAlg
+        Adds a new algorithm into a box
+        Receives the index of the box to insert into
+        Receives the algorithm to insert
+        """
+        self.boxes[boxIndex].add(alg)
+
+    def _removeAlg(self):
+        """
+        _removeAlg
+        Selects an algorithm to move back into box 0
+        Prefers to pick from the lowest box possible, can't pick from box 4
+        """
+        if self.boxes[1].length() != 0:
+            return self.boxes[1].getRandomAlg()
+        if self.boxes[2].length() != 0:
+            return self.boxes[2].getAlgorithm()
+        return self.boxes[3].getAlgorithm()
+
+    def _review_session(self):
         pass
+
+    def _algorithmsInRotation(self):
+        """
+        _algorithmsInRotation
+        Returns the number of algorithms that are currently being learned.
+        These are the algorithms in boxes 1, 2, and 3
+        """
+        return sum(self.boxes[1].length(), self.boxes[2].length(), self.boxes[3].length())
