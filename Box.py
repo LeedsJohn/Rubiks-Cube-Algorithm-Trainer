ab6d69ffe7b0b808a4563db1a59b8e66759175d2
John Leeds <leedsjohn6@gmail.com>
2022-05-28 17:15:58 -0500

    Added getAlgorithm and getFirstAlogorithmName

--- Box.py
+++ Box.py
@@ -5,6 +5,7 @@ Box.py
 
 Contains datastructure Box which holds Algorithms
 """
+import random
 import Algorithm
 
 class Box:
@@ -32,3 +33,19 @@ class Box:
         Returns the number of algorithms in the box
         """
         return len(self.algorithms)
+
+    def getAlgorithm(self):
+        """
+        getAlgorithm
+        Returns a randomly selected algorithm
+        """
+        return random.choice(self.algorithms)
+
+    def getFirstAlgorithmName(self):
+        """
+        getFirstAlgorithmName
+        Returns the name of the first algorithm from the algorithm list
+        """
+        if self.algorithms:
+            return self.algorithms[0].getName()
+        return ""
